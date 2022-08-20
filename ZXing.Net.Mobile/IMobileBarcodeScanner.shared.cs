9b4b52440e8c2ac6aaa31adb0d2e67cac3b0130c
attila.zabolai <attila.zabolai@dyntell.com>
2022-08-20 22:19:05 +0200

    formating

--- ZXing.Net.Mobile/IMobileBarcodeScanner.shared.cs
+++ ZXing.Net.Mobile/IMobileBarcodeScanner.shared.cs
@@ -1,90 +1,94 @@
 using System;
 using System.Threading.Tasks;
-using ZXing;
 
 namespace ZXing.Mobile
 {
-	public interface IZXingScanner<TOverlayViewType> : IScannerView
-	{
-		TOverlayViewType CustomOverlayView { get; set; }
-		bool UseCustomOverlayView { get; set; }
-		string TopText { get; set; }
-		string BottomText { get; set; }
-	}
+    public interface IZXingScanner<TOverlayViewType> : IScannerView
+    {
+        TOverlayViewType CustomOverlayView { get; set; }
+        bool UseCustomOverlayView { get; set; }
+        string TopText { get; set; }
+        string BottomText { get; set; }
+    }
 
-	public interface IMobileBarcodeScanner
-	{
-		Task<Result> Scan(MobileBarcodeScanningOptions options);
-		Task<Result> Scan();
+    public interface IMobileBarcodeScanner
+    {
+        Task<Result> Scan(MobileBarcodeScanningOptions options);
+        Task<Result> Scan();
 
-		void ScanContinuously(MobileBarcodeScanningOptions options, Action<Result> scanHandler);
-		void ScanContinuously(Action<Result> scanHandler);
+        void ScanContinuously(MobileBarcodeScanningOptions options, Action<Result> scanHandler);
+        void ScanContinuously(Action<Result> scanHandler);
 
-		void Cancel();
+        void Cancel();
 
-		void Torch(bool on);
-		void AutoFocus();
-		void ToggleTorch();
+        void Torch(bool on);
+        void AutoFocus();
+        void ToggleTorch();
 
-		void PauseAnalysis();
-		void ResumeAnalysis();
+        void PauseAnalysis();
+        void ResumeAnalysis();
 
-		bool UseCustomOverlay { get; }
-		string TopText { get; set; }
-		string BottomText { get; set; }
+        bool UseCustomOverlay { get; }
+        string TopText { get; set; }
+        string BottomText { get; set; }
 
-		string CancelButtonText { get; set; }
-		string FlashButtonText { get; set; }
-		string CameraUnsupportedMessage { get; set; }
+        string CancelButtonText { get; set; }
+        string FlashButtonText { get; set; }
+        string CameraUnsupportedMessage { get; set; }
 
-		bool IsTorchOn { get; }
-	}
+        bool IsTorchOn { get; }
+    }
 
-	public abstract class MobileBarcodeScannerBase : IMobileBarcodeScanner
-	{
-		public MobileBarcodeScannerBase()
-		{
-			CancelButtonText = "Cancel";
-			FlashButtonText = "Flash";
-			CameraUnsupportedMessage = "Unable to start Camera for Scanning";
-		}
+    public abstract class MobileBarcodeScannerBase : IMobileBarcodeScanner
+    {
+        public MobileBarcodeScannerBase()
+        {
+            CancelButtonText = "Cancel";
+            FlashButtonText = "Flash";
+            CameraUnsupportedMessage = "Unable to start Camera for Scanning";
+        }
 
-		public bool UseCustomOverlay { get; set; }
-		public string TopText { get; set; }
-		public string BottomText { get; set; }
-		public string CancelButtonText { get; set; }
-		public string FlashButtonText { get; set; }
-		public string CameraUnsupportedMessage { get; set; }
+        public bool UseCustomOverlay { get; set; }
 
-		public abstract Task<Result> Scan(MobileBarcodeScanningOptions options);
+        public string TopText { get; set; }
 
-		public Task<Result> Scan()
-			=> Scan(MobileBarcodeScanningOptions.Default);
+        public string BottomText { get; set; }
 
-		public void ScanContinuously(Action<Result> scanHandler)
-			=> ScanContinuously(MobileBarcodeScanningOptions.Default, scanHandler);
+        public string CancelButtonText { get; set; }
 
-		public abstract void ScanContinuously(MobileBarcodeScanningOptions options, Action<Result> scanHandler);
+        public string FlashButtonText { get; set; }
 
-		public abstract void Cancel();
+        public string CameraUnsupportedMessage { get; set; }
 
-		public abstract void Torch(bool on);
+        public abstract Task<Result> Scan(MobileBarcodeScanningOptions options);
 
-		public abstract void ToggleTorch();
+        public Task<Result> Scan()
+            => Scan(MobileBarcodeScanningOptions.Default);
 
-		public abstract bool IsTorchOn { get; }
+        public void ScanContinuously(Action<Result> scanHandler)
+            => ScanContinuously(MobileBarcodeScanningOptions.Default, scanHandler);
 
-		public abstract void AutoFocus();
+        public abstract void ScanContinuously(MobileBarcodeScanningOptions options, Action<Result> scanHandler);
 
-		public abstract void PauseAnalysis();
-		public abstract void ResumeAnalysis();
-	}
+        public abstract void Cancel();
 
-	public class CancelScanRequestEventArgs : EventArgs
-	{
-		public CancelScanRequestEventArgs()
-			=> Cancel = false;
+        public abstract void Torch(bool on);
 
-		public bool Cancel { get; set; }
-	}
+        public abstract void ToggleTorch();
+
+        public abstract bool IsTorchOn { get; }
+
+        public abstract void AutoFocus();
+
+        public abstract void PauseAnalysis();
+
+        public abstract void ResumeAnalysis();
+    }
+
+    public class CancelScanRequestEventArgs : EventArgs
+    {
+        public CancelScanRequestEventArgs() => Cancel = false;
+
+        public bool Cancel { get; set; }
+    }
 }
